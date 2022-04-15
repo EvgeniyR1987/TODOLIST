8d3e2be2553fa1e7e276bba089ca3302b906e291
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-04-15 22:09:14 +0300

    Update vue3.js

--- vue3.js
+++ vue3.js
@@ -6,15 +6,26 @@ Vue.createApp({
           notes: []
         }
     },
-    methods: {
-        inputchange(event){
-          this.inputvalue=event.target.value
+    mounted() {
+        this.notes = JSON.parse(localStorage.getItem('note')) || [];
+      },
+      watch: {
+            notes: {
+                handler: function() {
+                    localStorage.setItem('note', JSON.stringify(this.notes));
+                },
+                deep: true
+            }
         },
+    methods: {
         addnewtask(){
-            this.notes.push(this.inputvalue)
-            this.inputvalue=''
+            if (this.inputvalue !== ''){
+                this.notes.push(this.inputvalue)
+                this.inputvalue=''
+            }
         },
         removetask(index){
+            if (confirm('Do you really want to delete?'))
             this.notes.splice(index, 1)
         }
     }
