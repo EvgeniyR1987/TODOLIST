8c5581511b5dc29f6e3a3fab4ba9162df6b1bbe4
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-04-30 21:26:56 +0300

    Update vue3.js

--- vue3.js
+++ vue3.js
@@ -1,32 +1,68 @@
 Vue.createApp({
-    data(){
-        return{
-          placeholder: 'Start typing',
-          inputvalue: '',
-          notes: []
-        }
-    },
-    mounted() {
-        this.notes = JSON.parse(localStorage.getItem('note')) || [];
-      },
-    watch: {
-            notes: {
-                handler: function() {
-                localStorage.setItem('note', JSON.stringify(this.notes));
-                },
-                deep: true
-            }
-    },
-    methods: {
-        addnewtask(){
-            if (this.inputvalue !== ''){
-                this.notes.push({title:this.inputvalue, checked:false})
-                this.inputvalue=''
-            }
-        },
-        removetask(index){
-            if (confirm('Do you really want to delete?'))
-            this.notes.splice(index, 1)
-        }
+  data() {
+    return {
+      placeholder: 'Start typing',
+      inputvalue: '',
+      notes: [],
+      activeEdit: null
     }
-}).mount(app)
+  },
+  watch: {
+    notes: {
+      handler: function() {
+        localStorage.setItem('note', JSON.stringify(this.notes));
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    this.initNotes();
+  },
+  methods: {
+    initNotes() {
+      const NOTE_FROM_LOCAL_STORAGE = localStorage.getItem('note');
+      if (NOTE_FROM_LOCAL_STORAGE) {
+        try {
+          this.notes = JSON.parse(NOTE_FROM_LOCAL_STORAGE);
+        } 
+        catch(err) {
+          console.err(err)
+        }
+      }
+    },
+
+    addnewtask() {
+      if (this.inputvalue !== '') {
+        this.notes.push({
+          title: this.inputvalue,
+          checked: false
+        })
+        this.inputvalue = ''
+      }
+    },
+
+    removetask(index) {
+      if (confirm('Do you really want to delete?'))
+        this.notes.splice(index, 1)
+    },
+
+    editNote(note, noteIndex) {
+      this.activeEdit = note;
+      this.setFocusToNoteInput(noteIndex);
+    },
+
+    setFocusToNoteInput(noteIndex) {
+      setTimeout(() => {
+        this.$refs.notes[noteIndex].focus();
+      });
+    },
+
+    doneEdit(note) {
+      if (!this.activeEdit) {
+        return
+      }
+      this.activeEdit = null
+      note.title = note.title.trim()
+    },
+  }
+}).mount('#app')
